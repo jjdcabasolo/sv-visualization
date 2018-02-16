8925bf63b92cdcd1860d3e3ea14ea8223ea5f564
jjdcabasolo <abasolojohnjourish@gmail.com>
2018-02-17 00:54:27 +0800

    Added sv files and night mode

--- js/sv-visualization.js
+++ js/sv-visualization.js
@@ -5,6 +5,7 @@
 // - file-related
 // const fileDirRefGenome = 'data/IRGSP-1.0_genome.fa';
 const fileDirRefGenome = 'data/dummyref.txt';
+const fileDirSVDel = 'data/dummysv-del.txt';
 // - svg-related
 const svg = d3.select('#sv-visualization');
 const svgTopMargin = 50; // in px
@@ -15,13 +16,18 @@ const flankingValue = 50; // in bp
 
 // GLOBAL VARIABLES
 let referenceGenome = [];
+let structuralVariants = {};
+let svToDraw = [];
+let frequency = {};
 let noOfChr = 0;
-let startChr = 0;
-let endChr = 0;
+let noOfClusters = 0;
+let startChr = 0; // in bp
+let endChr = 0; // in bp
 // - svg-related
 let maxChrBound = 0;
-let rulerInterval = 20;
+let rulerInterval = 20; // in px
 let showSequence = true;
+let nightMode = false;
 let panZoomSVG = null;
 let zoom = true;
 let pan = true;
@@ -47,16 +53,22 @@ function readTextFile(){
 		noOfChr = referenceGenomeArray.length;
 
 		constructReferenceGenome(referenceGenomeArray);
-		// renderVisualization();
 		initializeSetCoordinates();
+	}, 'text');
+
+	$.get(fileDirSVDel, function(data) {
+		let svArray = [];
+		svArray = data.split(/\n/g);
+
+		constructStructuralVariants(svArray);
 	}, 'text');
 }
 
-// creates the referenceGenome object @ readTextFile()
+// creates the referenceGenome array @ readTextFile()
 function constructReferenceGenome(array){
 	let chrList = '';
 
-	for(let i = 0; i < noOfChr; i++){
+	for(var i = 0; i < noOfChr; i++){
 		let chrObj = {};	
 		chrObj['num'] = i + 1;
 		chrObj['seq'] = array[i];
@@ -70,11 +82,35 @@ function constructReferenceGenome(array){
 	$('#chr-num-select').append(chrList);
 }
 
+// creates the structural variant object @ readTextFile()
+function constructStructuralVariants(array){
+	frequency = {'INS':{}, 'DEL':{}, 'INV':{}, 'DUP':{}};
+	structuralVariants = {'INS':[], 'DEL':[], 'INV':[], 'DUP':[]};
+
+	for(var i = 0; i < array.length; i++){
+		let sv = array[i].split(/\t/g),
+				typeLen = sv[3].split(/;/g), // [0] == type, [1] == length
+				svObj = {};
+
+		svObj['chrNum'] = Number(sv[0].replace(/chr/g, ''));
+		svObj['start'] = Number(sv[1]);
+		svObj['end'] = Number(sv[2]);
+		svObj['length'] = Number(typeLen[1]);
+		svObj['detail'] = sv[4];
+		svObj['cluster'] = Number(sv[5]);
+
+		frequency[typeLen[0]][Number(sv[5])] = 1 + (frequency[typeLen[0]][Number(sv[5])] || 0);
+
+		structuralVariants[typeLen[0]][structuralVariants[typeLen[0]].length] = svObj;
+	}
+}
+
 // draw the visualization on the SVG [D3] @ readTextFile()
 function renderVisualization(){
 	svg.selectAll('*').remove(); // clear svg
 	drawRuler();
 	drawReferenceGenome();
+	drawStructuralVariants();
 	// enablePanAndZoom();
 }
 
@@ -87,7 +123,7 @@ function drawRuler(){
 			oneIndexingAdjustment = 2;
 	maxChrBound = currentChr.len * 7;
 
-	chrSequence = setSequenceRange(chrSequence);
+	chrSequence = setSequenceRange(chrSequence, true);
 
 	// draws the horizontal ruler
 	svg
@@ -108,8 +144,10 @@ function drawRuler(){
 }
 
 	// sets the sequence range to be displayed @ drawRuler()
-	function setSequenceRange(sequence){
-		$('#max-count').text(maxChrBound/7);
+	function setSequenceRange(sequence, replace){
+		if(replace){
+			$('#max-count').text(maxChrBound/7);
+		}
 
 		startChr = Number($('#chr-start').val() - 1);
 		endChr = Number($('#chr-end').val() - 1);
@@ -167,6 +205,8 @@ function drawRuler(){
 		// draws the sequence text
 		if(showSequence){
 		  svg
+		  	.append('g')
+		  		.attr('class', 'sequence-text')
 		  	.append('text')
 					.attr('x', 0 + svgLeftMargin)
 					.attr('y', svgTopMargin + 25)
@@ -195,6 +235,8 @@ function drawReferenceGenome(){
 
 	// the reference genome line
 	svg
+		.append('g')
+			.attr('class', 'reference-genome-line')
 		.append('rect')
 			.attr('x', 0 + svgLeftMargin)
 			.attr('y', svgTopMargin + 30)
@@ -208,19 +250,25 @@ function drawReferenceGenome(){
     .on('dblclick', trackDoubleClick);
 }
 
-// highlight effect on hover [D3] @ drawReferenceGenome()
-function trackMouseOver() {
-  d3.select(this).style('fill', 'url(#diagonalHatch)');
-}
+	// highlight effect on hover [D3] @ drawReferenceGenome()
+	function trackMouseOver(){
+	  d3.select(this).style('fill', 'url(#diagonalHatch)');
+	}
 
-// returns the previous color of the highlighted track [D3] @ drawReferenceGenome()
-function trackMouseOut() {
-  const color = d3.select(this).attr('color');
-  d3.select(this).style('fill', color);
-}
+	// returns the previous color of the highlighted track [D3] @ drawReferenceGenome()
+	function trackMouseOut(){
+	  const color = d3.select(this).attr('color');
+	  d3.select(this).style('fill', color);
+	}
 
-function trackDoubleClick() {
-	// baka lang may gawin ka pa, like additional modal zzzz
+	// @ drawReferenceGenome()
+	function trackDoubleClick(){
+		// baka lang may gawin ka pa, like additional modal zzzz
+	}
+
+// draws the sv's within the specified range @ renderVisualization()
+function drawStructuralVariants(){
+
 }
 
 // enables pan and zoom for the whole SVG [jQuery plugin] @ renderVisualization()
@@ -286,25 +334,33 @@ function enablePanAndZoom(){
 function initializeActionListeners(){
 	// reloads the visualization on chr change
 	$('#chr-num-select').on('change', function(){
-		var chrNum = Number($('#chr-num-select').val()) - 1,
-			currentChr = referenceGenome[chrNum];
+		let chrNum = Number($('#chr-num-select').val()) - 1,
+				currentChr = referenceGenome[chrNum];
 
 		$('#max-count').text(currentChr.len);
 	});
 
 	// toggles the sequence text
 	$('#toggle-seq-text').on('change', function(){
-		// if($('#toggle-seq-text').is(':checked') === true){
-		// 	showSequence = false;
-		// }
-		// else{
-		// 	showSequence = true;	
-		// }
-		// renderVisualization();
+		svg.selectAll('.sequence-text').remove(); // removes the sequence text
+
+		let chrNum = Number($('#chr-num-select option:selected').val()) - 1,
+				currentChr = referenceGenome[chrNum],
+				chrSequence = currentChr.seq.toUpperCase();
+
+		console.log('asdf')
+		if($('#toggle-seq-text').is(':checked') === true){
+			showSequence = false;
+		}
+		else{
+			showSequence = true;	
+		}
+
+		addSequenceText(setSequenceRange(chrSequence, false));
 	});
 
 	$('#ruler-interval').on('change', function(){
-		svg.selectAll('.ruler').remove(); // clear svg
+		svg.selectAll('.ruler').remove(); // removes the whole ruler
 		drawRuler();
 	});
 
@@ -317,6 +373,25 @@ function initializeActionListeners(){
 			$('#svg-holder').css('width', '98%'); 
 		}
 	});
+
+	// toggles app night mode
+	$('#toggle-night-mode').on('change', function(){
+		nightMode = ($('#toggle-night-mode').is(':checked') ? false : true);
+
+		if(!nightMode){
+			$('.night-mode').addClass('inverted');
+			$('.ui.toggle.checkbox input:focus:checked~label, .ui.toggle.checkbox input~label')
+				.attr('style', 'color: rgba(255,255,255,.9) !important');
+			$('.pusher').attr('style', 'background-color: rgba(255,255,255,.15) !important');
+		}
+		else{
+			$('.night-mode').removeClass('inverted');
+			$('.ui.toggle.checkbox input:focus:checked~label, .ui.toggle.checkbox input~label')
+				.attr('style', 'color: rgba(0,0,0,.87) !important');
+			$('.pusher').attr('style', 'background-color: #EEEEEE !important');
+		}
+		nightMode = !nightMode;
+	});
 }
 
 // initializes the set coordinates event listener on the button after reading the data @ readTextFile()
@@ -328,25 +403,113 @@ function initializeSetCoordinates(){
 
 		let startChr = Number($('#chr-start').val()) - 1,
 				endChr = Number($('#chr-end').val()) - 1,
-				maxBound = Number($('#max-count').text());
-
+				maxBound = Number($('#max-count').text()),
+				errorRange = false, errorSV = false,
+				svType = $('input[name=sv-type]:checked').val();
+		
 		if(maxBound < endChr){
 			// checks if the upper bound exceeds the length of the current chromosome
 			$('#range-message').text('The end range exceeds the length of the chromosome.');
+			errorRange = true;
 		}
-		else if(startChr > endChr){
+		if(startChr > endChr){
 			// checks if the lower bound is greater than the lower bound
 			$('#range-message').text('Start range is larger than end range.');
+			errorRange = true;
 		}
-		else if((endChr - startChr) < minimumRange){
+		if((endChr - startChr) < minimumRange){
 			// checks if the ranges is greater than the set minimum range
 			$('#range-message').text('Check the range. The minimum range is ' + minimumRange + '.');
+			errorRange = true;
 		}
-		else{
-			renderVisualization();
+		if(startChr == -1){
+			// checks if the start range input fields are empty
+			$('#range-message').text('Start range has no value.');
+			errorRange = true;
+		}
+		else if(endChr == -1){
+			// checks if the end range input fields are empty
+			$('#range-message').text('End range has no value.');
+			errorRange = true;
+		}
+		if(svType == null){
+			$('#sv-message').text('Pick a structural variant type to visualize.');
+			errorSV = true;
+		}
+
+		if(errorRange){
+			$('#dismissible-error-range').removeClass('hidden').transition('pulse');
+		}
+		if(errorSV){
+			$('#dismissible-error-sv').removeClass('hidden').transition('pulse');
+		}
+
+		if(!errorRange && !errorSV){
+			$('#dismissible-error-range').addClass('hidden');
+			$('#dismissible-error-sv').addClass('hidden');
+			if(checkSVExistence()){
+				renderVisualization();
+			}
 		}
 	});	
 }
+
+	// draws the structural variants inside the queried start and end @ initializeSetCoordinates()
+	function checkSVExistence(){
+		let startChr = Number($('#chr-start').val()),
+				endChr = Number($('#chr-end').val()),
+				chrNum = Number($('#chr-num-select').val()),
+				svType = $('input[name=sv-type]:checked').val();
+		svToDraw = filterSV();
+
+		if(svToDraw.length == 0){
+			switch(svType){
+	    case 'DEL':
+	        svType = 'deletions';
+	        break;
+	    case 'INV':
+	        svType = 'inversions';
+	        break;
+	    case 'DUP':
+	        svType = 'duplications';
+	        break;
+	    case 'INS':
+	        svType = 'insertions';
+	        break;
+	    default:
+	        svType = 'magical SV\'s';
+			}
+
+			// sets the message for the error, automatically closes after 10 seconds
+			$('#query-header').text('There are no ' + svType + ' found from ' + startChr + 'bp - ' + endChr + 'bp at chromosome number ' + chrNum + '.');
+			$('#dismissible-error-query').removeClass('hidden').transition('pulse');
+			$('#dismissible-error-query').delay(10000).fadeOut(400);
+
+			return false;
+		}	
+
+		return true;
+	}
+
+		// collects all the sv's inside the range quried by user @ checkSVExistence()
+		function filterSV(){
+			let startChr = Number($('#chr-start').val()) - 1,
+					endChr = Number($('#chr-end').val()) - 1,
+					svType = $('input[name=sv-type]:checked').val(),
+					svArray = structuralVariants[svType],
+					returnArray = [];
+			
+			for(var i = 0; i < svArray.length; i++){
+				let start = svArray[i]['start'],
+						end = svArray[i]['end'];
+
+				if(startChr <= start && endChr >= end){
+					returnArray[returnArray.length] = svArray[i];
+				}
+			}
+
+			return(returnArray);
+		}
 
 // HTML DOM manipulations [jQuery] @ initialize()
 function changeHTMLDOM(){	
@@ -372,16 +535,12 @@ function setupSemantic(){
 	// makes the option at the sidebar disappear on sidebar close
 	$('#left-sidebar-toggle').on('click', function(){
 		if(showOptions){
-			$('a#left-sidebar-query').css({'-webkit-transform':'translate(-100px, 0px)'});
-			$('a#left-sidebar-settings').css({'-webkit-transform':'translate(-100px, 0px)'});
-			$('a#left-sidebar-details').css({'-webkit-transform':'translate(-100px, 0px)'});
 			$('#return-options').css({'-webkit-transform':'translate(0px, -265px)'});
+			$('#option-toggle').addClass('sidebar').removeClass('window close').transition({animation: 'flash', duration: '0.4s'});
 		}
 		else{
-			$('a#left-sidebar-query').css({'-webkit-transform':'translate(0px, 0px)'});
-			$('a#left-sidebar-settings').css({'-webkit-transform':'translate(0px, 0px)'});
-			$('a#left-sidebar-details').css({'-webkit-transform':'translate(0px, 0px)'});
 			$('#return-options').css({'-webkit-transform':'translate(0px, 0px)'});
+			$('#option-toggle').addClass('window close').removeClass('sidebar').transition({animation: 'flash', duration: '0.4s'});
 		}
 		showOptions = !showOptions;
 	});
